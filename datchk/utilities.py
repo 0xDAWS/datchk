c8ffc6a88dd7b471685e3091896882520abb44d4
0xDAWS <81747134+0xDAWS@users.noreply.github.com>
2022-07-23 19:05:14 +0930

    Create utilities.py

---
+++ datchk/utilities.py
@@ -0,0 +1,35 @@
+import hashlib
+from pathlib import Path
+from py7zr import SevenZipFile
+from zipfile import ZipFile
+
+def extract_archive(rom_path, tmpdir) -> str:
+    if Path(rom_path).suffix == '.zip':
+        with ZipFile(rom_path, 'r') as z:
+            rom_name=z.namelist()[0]
+            z.extract(rom_name, tmpdir.name)
+
+    elif Path(rom_path).suffix == '.7z':
+        with SevenZipFile(rom_path, 'r') as z:
+            rom_name=z.files.files_list[0]['filename']
+            z.extract(path=tmpdir.name)
+
+    return rom_name
+
+def get_digest(algorithm, rom_path, tmp_dir) -> str:
+    h = hashlib.new(algorithm)
+    
+    if Path(rom_path).suffix in ['.zip','.7z']:
+        rom_name = extract_archive(rom_path, tmp_dir)
+
+        with open( Path(tmp_dir.name, rom_name), 'rb') as f:
+            h.update(f.read())
+
+    else:
+        with open(rom_path, 'rb') as f:
+            h.update(f.read())
+    
+    return h.hexdigest()
+
+def compare_rom_checksum(algoritm, rom_path, rom_digest, tmpdir) -> bool:
+    return get_digest(algoritm,rom_path,tmpdir) == rom_digest
